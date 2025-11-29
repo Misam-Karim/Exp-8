e80133ec3c8f54183e4304ed87ef067405da572f
Misam-Karim <misamkarim@eng.rizvi.edu.in>
2025-11-29 19:58:45 +0530

    Create E8task1.c

---
+++ E8task1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+    /*Misam Karim
+      UIN: 251P048
+      Class: Computer Engineering
+      Division: C */
+int getStringLength(char str[]) {
+    int length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
+    return length;
+}
+int main() {
+    char String[100];
+    printf("Enter a string: ");
+    fgets(myString, 100, stdin);
+    // Call the custom function
+    int len = getStringLength(String);
+    printf("The length of the string is: %d\n", len);
+    return 0;
+}
